50b13c6daa8c026b21d5dd8a6d3d51169319f576
engjoao <aquinosilva@gmail.com>
2016-09-20 15:02:17 -0300

    Add Navigation and Associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920172139) do
+ActiveRecord::Schema.define(version: 20160920175620) do
 
   create_table "plays", force: true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160920172139) do
     t.string   "director"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
