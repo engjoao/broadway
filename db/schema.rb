dbb08930c49ff649090234727121ecba61af5258
engjoao <aquinosilva@gmail.com>
2016-09-21 16:07:56 -0300

    Add Paperclip to App and Change View Files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920184356) do
+ActiveRecord::Schema.define(version: 20160921185553) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 20160920184356) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "category_id"
+    t.string   "play_img_file_name"
+    t.string   "play_img_content_type"
+    t.integer  "play_img_file_size"
+    t.datetime "play_img_updated_at"
   end
 
   create_table "users", force: true do |t|
